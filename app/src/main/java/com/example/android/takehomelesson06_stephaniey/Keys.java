9b886ce721afb7cc4223ef7725007698cf83f242
stephanieyang <stephanie.yang.121@gmail.com>
2018-03-05 17:22:05 -0500

    app layout, keys file

---
+++ app/src/main/java/com/example/android/takehomelesson06_stephaniey/Keys.java
@@ -0,0 +1,11 @@
+package com.example.android.takehomelesson06_stephaniey;
+
+/**
+ * Created by Stephanie on 3/5/2018.
+ */
+
+public class Keys {
+    String AMT_KEY = "amt";
+    String TAX_KEY = "tax";
+    String TIP_KEY = "tip";
+}
